03230916b6e17a81aef0cc5d519d9ac161485525
lubeve <lucasbenac@gmail.com>
2025-11-02 19:19:02 +0100

    feat: Update models init file with imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,17 @@
-
+\"\"\"
 Data models for the Telegram Insider Trading Bot.
+\"\"\"
+
+from .trading_opportunity import TradingOpportunity
+from .user_trading_settings import UserTradingSettings
+from .order_history import OrderHistory
+from .market_data_cache import MarketDataCache
+from .insider_activities import InsiderActivity
+
+__all__ = [
+    \"TradingOpportunity\",
+    \"UserTradingSettings\",
+    \"OrderHistory\",
+    \"MarketDataCache\",
+    \"InsiderActivity\"
+]
